bfac680127fac88c3160d0ab34f81070190668fe
FahrialRamadhan <fahrialg.o10@gmail.com>
2026-04-08 12:27:10 +0700

    melanjutkan dashboard

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,6 +31,62 @@
             </div>
         </div>
     </section>
+    <section class="max-w-6xl mx-auto px-6 mt-20">
 
+        <div class="grid md:grid-cols-2 gap-10 items-center">
 
+            <!-- LEFT CONTENT -->
+            <div>
+
+                <div class="flex items-center gap-2 mb-2">
+                    <img src="{{ asset('images/frameworkpartikel.png') }}" class="w-4 h-4">
+                    <p class="text-sm text-gray-500 uppercase tracking-wide">
+                        Tentang Kami
+                    </p>
+                </div>
+
+                <h2 class="text-2xl md:text-3xl font-semibold text-gray-800 leading-snug">
+                    Kadang, tempat terbaik itu nggak perlu dicari jauh-jauh.
+                </h2>
+
+                <p class="mt-2 text-gray-600">
+                    Hunian nyaman bisa jadi lebih dekat dari yang kamu kira.
+                </p>
+
+                <p class="mt-2 text-blue-500 font-medium">
+                    #RAFAKOST
+                </p>
+
+                <!-- Images -->
+                <div class="flex gap-4 mt-6">
+
+                    <img src="{{ asset('images/koridorkamarkiri.png') }}" class="w-1/2 h-40 object-cover rounded-lg">
+
+                    <img src="{{ asset('images/koridorkamarkanan.png') }}" class="w-1/2 h-40 object-cover rounded-lg">
+
+                </div>
+
+            </div>
+
+            <!-- RIGHT CONTENT -->
+            <div class="relative w-full opacity-60">
+
+                <!-- ICON (pojok kanan section, bukan text) -->
+                <img src="{{ asset('images/bintangpartikel.png') }}"
+                    class="absolute top-[-20px] right-0 w-6 h-6 opacity-70">
+
+                <!-- TEXT -->
+                <div class="text-gray-600 leading-relaxed text-sm md:text-base max-w-xl justify-start p-50">
+                    <p>
+                        <span class="text-blue-500 font-medium">Rafa Kost</span> hadir dengan fasilitas lengkap,
+                        lingkungan aman, dan lokasi strategis untuk hunian nyaman tanpa ribet—
+                        cocok untuk mahasiswa maupun pekerja.
+                    </p>
+                </div>
+
+            </div>
+
+        </div>
+
+    </section>
 </x-guest-layout>
